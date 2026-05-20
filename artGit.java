2123c50a0c1f9565a814b0fc3be9ff3f81b8a8f2
Artin-Etaati <artinetaati@gmail.com>
2026-05-19 17:06:57 -0700

    pushing using lap top for the first time

--- artGit.java
+++ artGit.java
@@ -7,5 +7,6 @@ public class artGit{
     System.out.println("hii");
     System.out.println("hi, how are you");
     System.out.println("a new branch has been added and merged");
+    System.out.println("pushing using my lap top");
   }
 }
